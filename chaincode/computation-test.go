8130cafb6cbf0bf06f419a401f1176f6c40f4540
yilisita <1363467835@qq.com>
2022-01-10 13:30:50 +0800

    provided data for test

--- chaincode/computation-test.go
+++ chaincode/computation-test.go
@@ -3,9 +3,11 @@ package chaincode
 import (
 	"fmt"
 	"math/big"
-	"math/rand"
-	"test-algorithm/encryption/paillier"
 	"time"
+
+	//"math/rand"
+	"test-algorithm/encryption/paillier"
+	//"time"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
@@ -24,51 +26,71 @@ func decrypt(n int64) int64 {
 	return paillier.Encryption(key, big.NewInt(n)).Int64()
 }
 
-var DATA_POOL []int64
-var SUM int64
+// func (p *PaillierContract) EncryptData(ctx contractapi.TransactionContextInterface, data *[]int64) int64 {
+// 	start := time.Now()
+// 	for i, v := range *data {
+// 		(*data)[i] = encrypt(v)
+// 	}
+// 	end := time.Since(start).Milliseconds()
+// 	return end
+// }
 
-// 生成待测试的数据
-// n: 表示生成多少条数据
-// bit: 表示生成的每条数据的位数，e.g. 两位数，三位数
-func (p *PaillierContract) GenerateData(ctx contractapi.TransactionContextInterface, n, bit int) {
-	// 生成之前先清除数据池中的数据
-	DATA_POOL = DATA_POOL[0:0]
-	// 在循环体外面播撒种子
-	rand.Seed(time.Now().Unix())
-	for i := 0; i < n; i++ {
-		temp := rand.Int63()
-		SUM += temp
-		DATA_POOL = append(DATA_POOL, encrypt(temp))
-	}
-}
-
-func (p *PaillierContract) TestAggregate(ctx contractapi.TransactionContextInterface) float64 {
+// 测试聚合运算，调用这个函数的时候测试数据也会被加密了
+// 参数：测试数据的索引值
+// 返回：返回加密过程的用时，聚合计算以及解密过程的总用时
+func (p *PaillierContract) TestAggregate(ctx contractapi.TransactionContextInterface, index int) []int64 {
 	fmt.Println("TestAggregate:------------------------------------------")
-	var LEN = len(DATA_POOL)
+	var data = DataIndex[index]
+	var LEN = len(data)
 	fmt.Println("测试数据量:", LEN)
 	fmt.Println("开始测试......")
-	fmt.Println("明文求和:", SUM)
+
+	// 明文求和
+	var sum int64 = 0
+	for _, v := range data {
+		sum += v
+	}
+	fmt.Println("明文求和:", sum)
+
+	// 计时加密
 	start := time.Now()
-	var res int64 = 0
-	for _, v := range DATA_POOL {
-		res += v
+	for i, v := range data {
+		data[i] = encrypt(v)
 	}
 	end := time.Since(start).Milliseconds()
-	fmt.Println("密文求和解密结果:", res)
-	return float64(end)
+
+	// 计算计时
+	start1 := time.Now()
+	var res int64 = 0
+	for _, v := range data {
+		res += v
+	}
+	end1 := time.Since(start1).Milliseconds()
+	fmt.Println("密文求和解密结果:", decrypt(res))
+	fmt.Println("用时(单位:ms):", end1)
+	return []int64{end, end1}
 }
 
-func (p *PaillierContract) TestNTimes(ctx contractapi.TransactionContextInterface, times, n, bit int) {
-	fmt.Println("TestNTimes:------------------------------------------")
-	for i := 0; i < times; i++ {
-		p.GenerateData(ctx, n, bit)
-		start := time.Now()
-		var res int64 = 0
-		for _, v := range DATA_POOL {
-			res += v
-		}
-		res = decrypt(res)
-		end := time.Since(start).Milliseconds()
-		fmt.Println(end)
+// func (p *PaillierContract) TestNTimes(ctx contractapi.TransactionContextInterface, times, n, bit int) {
+// 	fmt.Println("TestNTimes:------------------------------------------")
+// 	for i := 0; i < times; i++ {
+// 		var DATA_POOL = p.GenerateData(ctx, n, bit)
+// 		start := time.Now()
+// 		var res int64 = 0
+// 		for _, v := range DATA_POOL {
+// 			res += v
+// 		}
+// 		res = decrypt(res)
+// 		end := time.Since(start).Milliseconds()
+// 		fmt.Println(end)
+// 	}
+// }
+
+func (p *PaillierContract) SayHi(ctx contractapi.TransactionContextInterface) string {
+	err := ctx.GetStub().PutState("1", []byte("Hi"))
+	if err != nil {
+		return "Error"
 	}
+	fmt.Println("Hi, from Here")
+	return "Hi"
 }
